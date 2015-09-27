3cafc1540869bd82b3bfd6e56c13d186b6a7cff7
joker1007 <kakyoin.hierophant@gmail.com>
2015-09-27 20:44:01 +0900

    Implement clear database

--- src/containers/menu.js
+++ src/containers/menu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import swal from 'sweetalert';
 import { requestUpdateDb, updateDb } from '../actions';
 import menuTemplate from './menu_template';
 
@@ -22,7 +23,25 @@ class AppMenu extends Component {
                 dispatch(updateDb(result.files));
               })
             }
-          }
+          },
+          {
+            label: 'Clear Database',
+            click: (item, focusedWindow) => {
+              swal({
+                title: "Are you sure?",
+                text: "You will not be able to recover database",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, Clear",
+                closeOnConfirm: true,
+                html: false,
+              }, () =>{
+                indexedDB.deleteDatabase("blackalbum");
+                window.location.reload();
+              });
+            }
+          },
         ]
       }
     ]);
